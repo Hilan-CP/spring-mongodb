0ecab809b43188588137066785edd0224db7ab71
Hilan-CP <hilan.paganini@gmail.com>
2022-09-01 20:24:12 -0300

    First Commit

---
+++ src/main/java/com/hilan/mongodbapplication/MongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.hilan.mongodbapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbApplication.class, args);
+	}
+
+}
