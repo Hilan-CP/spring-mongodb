bbef08f69642f009b95f5759826e7ccc2b48131e
Hilan-CP <hilan.paganini@gmail.com>
2022-09-21 20:39:43 -0300

    Consulta com vários critérios

--- src/main/java/com/hilan/mongodbapplication/resources/PostResource.java
+++ src/main/java/com/hilan/mongodbapplication/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.hilan.mongodbapplication.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,17 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
 		text = URLString.decodeParam(text);
 		List<Post> posts = service.findByTitle(text);
+		return ResponseEntity.ok(posts);
+	}
+	
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> searchTextBetweenDate(@RequestParam(value="text", defaultValue="") String text,
+															@RequestParam(value="minDate", defaultValue="") String minDate,
+															@RequestParam(value="maxDate", defaultValue="") String maxDate){
+		text = URLString.decodeParam(text);
+		Date min = URLString.convertDate(minDate, new Date(0L));
+		Date max = URLString.convertDate(maxDate, new Date());
+		List<Post> posts = service.searchTextBetweenDate(text, min, max);
 		return ResponseEntity.ok(posts);
 	}
 }
